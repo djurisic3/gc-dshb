73665e86b3109506de2deeb83270603e1482ec1d
Dario Jurisic <dariojrsc@gmail.com>
2025-08-16 14:11:13 +0200

    check

--- index.js
+++ index.js
@@ -105,18 +105,8 @@ app.listen(PORT, () => {
   console.log(`Server radi na portu ${PORT}`);
 });
 
- const path = require('path');
 
-// Serve static files from React build
-app.use(express.static(path.join(__dirname, 'frontend', 'build')));
-
-console.log("before catch-all");
-// Sve ostale GET rute šalju index.html (React SPA)
-app.get('/*path', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-});
-
-/* const fs = require('fs');
+const fs = require('fs');
 
 function walkDir(dir, filelist = []) {
   const files = fs.readdirSync(dir);
@@ -138,4 +128,15 @@ app.get('/debug/all-files', (req, res) => {
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
-}); */
+});
+
+ const path = require('path');
+
+// Serve static files from React build
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+console.log("before catch-all");
+// Sve ostale GET rute šalju index.html (React SPA)
+app.get('/*path', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
